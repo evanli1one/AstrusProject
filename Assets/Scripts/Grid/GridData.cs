dfae2392708421f3a1403e1644851d9a11ee854b
Evan Li <eli24@ucsc.edu>
2021-07-09 18:22:58 -0700

    Added grid info tiles

--- Assets/Scripts/Grid/GridData.cs
+++ Assets/Scripts/Grid/GridData.cs
@@ -12,20 +12,6 @@ public class GridData: MonoBehaviour
         BoundsInt bounds = dataTilemap.cellBounds;
         TileBase[] allTiles = dataTilemap.GetTilesBlock(bounds);
 
-        for (int x = 0; x < bounds.size.x; x++)
-        {
-            for (int y = 0; y < bounds.size.y; y++)
-            {
-                TileBase tile = allTiles[x + y * bounds.size.x];
-                if (tile != null)
-                {
-                    Debug.Log("x:" + x + " y:" + y + " tile:" + tile.name);
-                }
-                else
-                {
-                    //Debug.Log("x:" + x + " y:" + y + " tile: (null)");
-                }
-            }
-        }
+        print("bounds: " + bounds);
     }
 }
